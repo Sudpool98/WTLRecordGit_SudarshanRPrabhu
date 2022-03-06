d26a9d69115ee6079e7950c2b769fbf943450708
Sudpool98 <97434942+Sudpool98@users.noreply.github.com>
2022-03-07 01:19:40 +0530

    PHP_Complete_7March2022
    Added 10 PHP Programs.

---
+++ PHP/Program14Array.php
@@ -0,0 +1,6 @@
+<?php    
+$skins=array("Prime"=>"1775","GlitchPop"=>"2045","ElderFlame"=>"2425","Spectrum"=>"2675");  
+foreach($skins as $k => $v) {  
+echo "Key: ".$k." Value: ".$v."<br/>";  
+}  
+?>   
---
+++ PHP/Program19FileCheck.php
@@ -0,0 +1,8 @@
+<?php
+$filename = "Program12Date.php";
+if (file_exists($filename)) {
+    echo "File exist.";
+} else {
+    echo "File does not exist.";
+}
+?>
